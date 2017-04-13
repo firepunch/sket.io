04db59abc3393ba1269ff2d3b0158f3fb48d0dd3
KwonJH <hojak99@gmail.com>
2017-04-13 12:27:09 +0900

    Add method to create a room
    RoomController 클래스에 메소드 추가 및 웹소켓에서 코드 작성 중

--- src/sket/controllers/RoomController.java
+++ src/sket/controllers/RoomController.java
@@ -1,10 +1,22 @@
 package sket.controllers;
 
-public class RoomController{
+import sket.model.data.Player;
+import sket.model.data.Room;
+import sket.model.data.User;
+
+import javax.websocket.Session;
+
+public class RoomController {
 
     public RoomController() {
         super();
     }
 
+    public static Room createRoom(String name, boolean islock, String pwd, Session session) {
+
+        // 방 생성 코드. Room 생성자 안에 roomList 에 방 추가하는 코드 작성되있음.
+        Room room = new Room(name, Player.getPlayerEqualSession(session), Room.getCountRoomId(), islock, pwd);
+        return room;
+    }
 
 }
