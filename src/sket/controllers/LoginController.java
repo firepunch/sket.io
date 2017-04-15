dc30d354c1788e2a78a445f891913eddbceaac1f
firepunch <alfla456@gmail.com>
2017-04-15 23:59:19 +0900

    Login FB using Oauth
    oauth를 이용한 페북로그인, 간단하게 값만 확인하는 것까지. url not found 해결해야 함

--- src/sket/controllers/LoginController.java
+++ src/sket/controllers/LoginController.java
@@ -1,31 +1,62 @@
 package sket.controllers;
 
-import sket.model.action.Authenticator;
-import sket.model.data.User;
+import sket.model.action.FBConnection;
+import sket.model.action.FBGraph;
+import sket.model.action.LoginGoogle;
 
-import javax.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.util.Map;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class LoginController extends HttpServlet {
+
     private static final long serialVersionUID = 1L;
+    private String code="";
 
     public LoginController() {
         super();
     }
 
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
 
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        String nick = request.getParameter("nick");
-        RequestDispatcher rd = null;
 
-        Authenticator authenticator = new Authenticator();
+        String act = req.getParameter("loginBtn");
+
+        System.out.println("31line");
+
+        if (act == null) {
+            //no button has been selected
+        } else if (act.equals("fb")) {
+            code = req.getParameter("code");
+            if (code == null || code.equals("")) {
+                throw new RuntimeException(
+                        "ERROR: Didn't get code parameter in callback.");
+            }
+            FBConnection fbConnection = new FBConnection();
+            String accessToken = fbConnection.getAccessToken(code);
+
+            FBGraph fbGraph = new FBGraph(accessToken);
+            String graph = fbGraph.getFBGraph();
+            Map<String, String> fbProfileData = fbGraph.getGraphData(graph);
+            ServletOutputStream out = res.getOutputStream();
+            out.println("<h1>Facebook Login using Java</h1>");
+            out.println("<h2>Application Main Menu</h2>");
+            out.println("<div>Welcome "+fbProfileData.get("first_name"));
+            out.println("<div>Your Email: "+fbProfileData.get("email"));
+            out.println("<div>Your Token: "+accessToken);
+        } else if (act.equals("google")) {
+//            LoginGoogle.loginGoogle = new LoginGoogle();
+        } else {
+            //someone has altered the HTML and sent a different value!
+        }
+
+/*        Authenticator authenticator = new Authenticator();
         String result = authenticator.authenticate(username, password);
         if (result.equals("success")) {
 //            rd = request.getRequestDispatcher("/success.jsp");
@@ -35,8 +66,7 @@ public class LoginController extends HttpServlet {
         } else {
             System.out.println("success fail");
 //            rd = request.getRequestDispatcher("/error.jsp");
-        }
-        rd.forward(request, response);
+        }*/
     }
 
     public void guestLogin() {
---
+++ src/sket/model/action/FBConnection.java
@@ -0,0 +1,83 @@
+package sket.model.action;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+/**
+ * Created by firepunch on 2017-03-26.
+ */
+
+public class FBConnection {
+    public static final String FB_APP_ID = "741189302727195";
+    public static final String FB_APP_SECRET = "66b7c9302459527e06c2501cc69fb78b";
+    public static final String REDIRECT_URI = "http://localhost:8080/signin/facebook";
+
+    static String accessToken = "";
+
+    public String getFBAuthUrl() {
+        String fbLoginUrl = "";
+        try {
+            fbLoginUrl = "http://www.facebook.com/dialog/oauth?" + "client_id="
+                    + FBConnection.FB_APP_ID + "&redirect_uri="
+                    + URLEncoder.encode(FBConnection.REDIRECT_URI, "UTF-8")
+                    + "&scope=email";
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return fbLoginUrl;
+    }
+
+    public String getFBGraphUrl(String code) {
+        String fbGraphUrl = "";
+        try {
+            fbGraphUrl = "https://graph.facebook.com/oauth/access_token?"
+                    + "client_id=" + FBConnection.FB_APP_ID + "&redirect_uri="
+                    + URLEncoder.encode(FBConnection.REDIRECT_URI, "UTF-8")
+                    + "&client_secret=" + FB_APP_SECRET + "&code=" + code;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return fbGraphUrl;
+    }
+
+    public String getAccessToken(String code) {
+        if ("".equals(accessToken)) {
+            URL fbGraphURL;
+            try {
+                fbGraphURL = new URL(getFBGraphUrl(code));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Invalid code received " + e);
+            }
+            URLConnection fbConnection;
+            StringBuffer b = null;
+            try {
+                fbConnection = fbGraphURL.openConnection();
+                BufferedReader in;
+                in = new BufferedReader(new InputStreamReader(
+                        fbConnection.getInputStream()));
+                String inputLine;
+                b = new StringBuffer();
+                while ((inputLine = in.readLine()) != null)
+                    b.append(inputLine + "\n");
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Unable to connect with Facebook "
+                        + e);
+            }
+
+            accessToken = b.toString();
+            if (accessToken.startsWith("{")) {
+                throw new RuntimeException("ERROR: Access Token Invalid: "
+                        + accessToken);
+            }
+        }
+        return accessToken;
+    }
+}
