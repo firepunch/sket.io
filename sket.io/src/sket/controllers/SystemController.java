beada8e5a686ddb6be3edc3eed02e6601327f1d6
firepunch <alfla456@gmail.com>
2017-05-04 15:02:10 +0900

    Delete another project folder
    포함되어 있던 다다른 sketio 폴더를 삭제

--- sket.io/src/sket/controllers/SystemController.java
+++
@@ -1,9 +0,0 @@
-package sket.controllers;
-
-import javax.servlet.http.HttpServlet;
-
-public class SystemController extends HttpServlet {
-    public SystemController() {
-        super();
-    }
-}
