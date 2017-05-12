f5386a0ce2dcaad2d4382214c7c34cf0ee5182a9
KwonJH <hojak99@gmail.com>
2017-05-12 15:53:23 +0900

    Add GetHttpSessionConfigurator class
    httpsession으로 변경해주는 클래스

--- src/sket/GetHttpSessionConfigurator.java
+++ src/sket/GetHttpSessionConfigurator.java
@@ -1,24 +1,20 @@
 package sket;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.websocket.HandshakeResponse;
 import javax.websocket.server.HandshakeRequest;
 import javax.websocket.server.ServerEndpointConfig;
 
-/**
- * Created by KwonJH on 2017-05-11.
- */
-
-// 웹 소켓 접속 시에 HTTP 세션 정보도 같이 전달해준다고 한다.
 public class GetHttpSessionConfigurator extends ServerEndpointConfig.Configurator {
 
     @Override
-    public void modifyHandshake(ServerEndpointConfig config,
-                                HandshakeRequest request,
-                                HandshakeResponse response) {
-        HttpSession httpSession = (HttpSession) request.getHttpSession();
-
-        if (httpSession != null)
-            config.getUserProperties().put(HttpSession.class.getName(), httpSession);
+    public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
+        try {
+            HttpSession session = (HttpSession) request.getHttpSession();
+            config.getUserProperties().put(HttpSession.class.getName(), session);
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
     }
 }
