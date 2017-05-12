0d3faadb5bd45f173e9762acd54746c88d54b743
KwonJH <hojak99@gmail.com>
2017-05-12 15:54:38 +0900

    Add create HttpSession and setAttribute

--- src/sket/controllers/SignUpController.java
+++ src/sket/controllers/SignUpController.java
@@ -1,9 +1,13 @@
 package sket.controllers;
 
+import sket.model.action.SessionManager;
+import sket.model.data.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -13,20 +17,21 @@ public class SignUpController extends HttpServlet {
 
     private String id;
     private String password;
+    private String nickname;
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("euc-kr");
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html");
 
-        System.out.println("signup.do");
-
         id = req.getParameter("id");
         password = req.getParameter("password");
+        nickname = req.getParameter("nick");
 
-             System.out.println("getId : "+id);
-        System.out.println("getPwd : "+password);
+        HttpSession session = req.getSession();
+        session.setAttribute("user", new User(id, password, nickname));
+        SessionManager.addSession(session);
     }
 }
 
