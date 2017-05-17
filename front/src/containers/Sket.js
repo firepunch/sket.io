97e51617e2cb5b6ff46334793fd39e312474210d
firepunch <cynne13@naver.com>
2017-05-17 23:52:40 +0900

    pull ready

--- front/src/containers/Sket.js
+++ front/src/containers/Sket.js
@@ -5,6 +5,7 @@ import { browserHistory } from 'react-router';
 // import { oauthActions, reducer, signin, signout } from 'react-redux-oauth2'
 // import { OAuthSignInButton } from "redux-auth/material-ui-theme";
 import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 
 
 import IndexContent from './IndexContent';
@@ -53,8 +54,8 @@ class Sket extends Component {
 
                         <a href="http://www.facebook.com/dialog/oauth?client_id=741189302727195&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fsignin%2Ffacebook%2F">
                             <button onClick={() => this.props.handleLogin('facebook')}
-                                className="action-button shadow animate blue"
-                                >
+                                    className="action-button shadow animate blue"
+                            >
                                 페이스북으로 로그인하기
                             </button>
                         </a>
@@ -63,8 +64,8 @@ class Sket extends Component {
                     <div className="login-button">
                         <a href="https://accounts.google.com/o/oauth2/auth?client_id=755801497962-25e8cmnp81pcld5r8mfsvmetus9qnnv4.apps.googleusercontent.com&redirect_uri=http://localhost:8080/signin/google/&scope=https://www.googleapis.com/auth/plus.login&response_type=code">
                             <button onClick={() => this.props.handleLogin('google')}
-                                className="action-button shadow animate red"
-                                >
+                                    className="action-button shadow animate red"
+                            >
                                 구글로 로그인하기
                             </button>
                         </a>
@@ -76,10 +77,16 @@ class Sket extends Component {
                         onSuccess={(res) => { console.log(res) }}
                         onFailure={(res) => { console.log(res) }}
                     />
+                    <FacebookLogin
+                        appId="741189302727195"
+                        autoLoad={true}
+                        fields="name,email,picture"
+                        callback={(res) => { console.log(res) }}
+                    />
 
                     <div className="login-button">
                         <button onClick={() => this.props.handleLogin('guest')}
-                            className="action-button shadow animate green"
+                                className="action-button shadow animate green"
                         >
                             GUEST로 로그인하기
                         </button>
@@ -90,11 +97,11 @@ class Sket extends Component {
         )
 
         const index = (
-                <IndexContent/>
+            <IndexContent/>
         )
 
         const game = (
-                <GameContent/>
+            <GameContent/>
         )
 
         // const content = (
