4f373d6b5311ae001b25f9ed197d8274ba1414d0
NCookies <swstar21c@gmail.com>
2017-05-24 23:36:49 +0900

    react-redux-modal 라이브러리를 이용한 모달 생성 성공

--- front/src/components/Index/FuncButtonArea.js
+++ front/src/components/Index/FuncButtonArea.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
+import { modal } from 'react-redux-modal';
 
 // import FuncButton from './FuncButton';
-import IndexModal from './createRoomModal';
 
 
 const propTypes = {
@@ -12,14 +12,46 @@ const defaultProps = {
     modalType: 0
 };
 
+
+class modalComponent extends Component {
+    constructor(props) {
+        super(props);
+        console.log('## MODAL DATA AND PROPS:', this.props);
+    }
+
+    removeThisModal() {
+        this.props.removeModal();
+    }
+
+    render() {
+        return (
+            <div>
+                <p>this is my modal</p>
+                <button
+                    type="button"
+                    onClick={this.removeThisModal.bind(this)}>
+                    close this modal
+                </button>
+            </div>
+        );
+    }
+}
+
 class FuncButtonArea extends Component {
     constructor(props) {
         super(props);
     }
 
-    // <FuncButton
-    //     modalType={this.props.modalType}/>
-    // <FuncButton/>
+    addModal() {
+        modal.add(modalComponent, {
+            title: '방 생성',
+            size: 'large', // large, medium or small,
+            closeOnOutsideClick: true, // (optional) Switch to true if you want to close the modal by clicking outside of it,
+            hideTitleBar: false, // (optional) Switch to true if do not want the default title bar and close button,
+            hideCloseButton: false // (optional) if you don't wanna show the top right close button
+            //.. all what you put in here you will get access in the modal props ;)
+        });
+    }
 
     render() {
         return(
@@ -27,19 +59,21 @@ class FuncButtonArea extends Component {
 
                 <div>
                     <div className="sket-button" id="craete-room">
-                        <createRoomModal
-                            modalName="방 생성"/>
+                        <button onClick={this.addModal.bind(this)}
+                                className="action-button shadow animate red">
+                            <p>방 생성</p>
+                        </button>
                     </div>
 
                     <div className="sket-button" id="quick-start">
                         <button className="action-button shadow animate red">
-                            빠른 시작
+                            <p>빠른 시작</p>
                         </button>
                     </div>
 
                     <div className="sket-button" id="ranking">
                         <button className="action-button shadow animate red">
-                            랭킹
+                            <p>랭킹</p>
                         </button>
                     </div>
                 </div>
