7548fd72c37012294d1f81d2239d9a5be2ab2bbe
firepunch <cynne13@naver.com>
2017-05-29 20:13:56 +0900

    delte session in logincontroller

--- src/sket/controllers/GoogleLoginController.java
+++ src/sket/controllers/GoogleLoginController.java
@@ -3,14 +3,11 @@ package sket.controllers;
 import org.json.JSONObject;
 import sket.db.DBConnection;
 import sket.model.action.OauthLogin;
-import sket.model.action.SessionManager;
-import sket.model.data.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -29,8 +26,6 @@ public class GoogleLoginController extends HttpServlet {
         DBConnection db = new DBConnection();
         OauthLogin oauthLogin = new OauthLogin();
         PrintWriter out = resp.getWriter();
-
-        HttpSession session = req.getSession();
         JSONObject sendJson = oauthLogin.getRcvJson(req, "google", "user");
 
         req.setCharacterEncoding("euc-kr");
@@ -60,16 +55,16 @@ public class GoogleLoginController extends HttpServlet {
             sendJson.put("totalExp", 0);
             sendJson.put("curExp", 0);
 
-            session.setAttribute("user", new User(id, nick));
-            SessionManager.addSession(session);
+//            session.setAttribute("user", new User(id, nick));
             System.out.println("log : " + "Google 새로운 세션, 신규회원 생성");
-        } else if (SessionManager.getUserIdEqualSession(session) == null) {
+        } else {
             System.out.println("log : Google 기존회원 새로운 세션 생성");
 
+/*
             session.setAttribute("user", new User(id, nick,
                     sendJson.getInt("level"), sendJson.getInt("limitExp"),
                     sendJson.getInt("totalExp"), sendJson.getInt("curExp")));
-            SessionManager.addSession(session);
+*/
         }
 
         out.print(sendJson);
