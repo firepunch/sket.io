b74d0e247a69bd5f10cdea7b5ce81e0b41bdff44
KwonJH <hojak99@gmail.com>
2017-06-01 20:18:34 +0900

    Response json (Guest)

--- src/sket/controllers/GuestController.java
+++ src/sket/controllers/GuestController.java
@@ -1,5 +1,6 @@
 package sket.controllers;
 
+import org.json.JSONObject;
 import sket.model.data.User;
 
 import javax.servlet.ServletException;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /*
 *
@@ -24,15 +26,29 @@ public class GuestController extends HttpServlet {
 
         req.setCharacterEncoding("euc-kr");
         resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html");
+        resp.setContentType("application/json");
+
+        JSONObject message = new JSONObject();
+        message.put("type", "LOGIN_GUEST");
+
+        JSONObject data = new JSONObject();
 
         try {
-            HttpSession session = req.getSession();
+            User user = new User(true);
+            data.put("id", user.getId());
+            data.put("nick", user.getNick());
+            data.put("level", user.getLevel());
+            data.put("totalExp", user.getTotalExp());
+            data.put("limitExp", user.getLimitExp());
 
-            // Guest 일 때는 생성자 인자로 true 를 줘야한다!
-            session.setAttribute("user", new User(true));
+            message.put("data", data);
 
             System.out.println("log : " + "게스트 로그인 성공!");
+
+            PrintWriter out = resp.getWriter();
+            out.print(message);
+            out.flush();
+
         } catch (Exception e) {
             System.out.println("log : " + "GuestController 오류");
             e.printStackTrace();
