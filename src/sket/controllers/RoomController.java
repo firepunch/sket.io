55d2d878db0cff33c5812f31bf250a6c1be8bc64
KwonJH <hojak99@gmail.com>
2017-06-02 09:30:33 +0900

    createRoom json 변경

--- src/sket/controllers/RoomController.java
+++ src/sket/controllers/RoomController.java
@@ -13,9 +13,9 @@ public class RoomController {
     }
 
     /* 방 생성하는 메소드 */
-    public static Room createRoom(String name, boolean isLock, String pwd, String masterId) {
+    public static Room createRoom(String name, boolean isLock, String pwd, String masterId, int userMax) {
         // 방 생성 코드. Room 생성자 안에 roomList 에 방 추가하는 코드 작성되있음.
-        Room room = new Room(name, PlayerAction.getEqualPlayerId(masterId), Room.getCountRoomId(), isLock, pwd);
+        Room room = new Room(name, PlayerAction.getEqualPlayerId(masterId), Room.getCountRoomId(), isLock, pwd, userMax);
         Player player = PlayerAction.getEqualPlayerId(masterId);
         player.setMaster(true);
 
@@ -48,13 +48,13 @@ public class RoomController {
 
     /* 방 목록을 json 으로 보내는 메소드 */
     public static String getRoomListAsJSON() {
+        System.out.println("log : 방 목록 리스트 사이즈 : " + Room.getRoomList().size());
+
         JSONObject message = new JSONObject();
         message.put("type", "ROOM_LIST");
 
         JSONObject data = new JSONObject();
         JSONArray jsonArray = new JSONArray();
-
-        System.out.println("log : 방 목록 리스트 사이즈 : " + Room.getRoomList().size());
 
         if (Room.getRoomList().size() == 0) {
             data.put("size", 0);
@@ -65,11 +65,13 @@ public class RoomController {
         for (Room room : Room.getRoomList()) {
             JSONObject object = new JSONObject();
             object.put("roomId", room.getRoomId());
-            object.put("name", room.getRoomName());
-            object.put("lock", room.isLocked());
-            object.put("password", room.getRoomPwd());
-            object.put("playerNumber", room.getTotalUserNumber());
-
+            object.put("roomName", room.getRoomName());
+            object.put("round", room.getRound());
+            object.put("timeLimit", room.getTimeLimit());
+            object.put("userNumLimit", room.getUserMax());
+            object.put("userNum", room.getTotalUserNumber());
+            object.put("isLocked", room.isLocked());
+            
             jsonArray.put(object);
         }
 
