31f68444f07aaba7577fe0b954d1cffe49790916
KwonJH <hojak99@gmail.com>
2017-06-08 22:54:33 +0900

    randomExaminerToJSON 메소드 매개변수 수정

--- src/sket/controllers/GameController.java
+++ src/sket/controllers/GameController.java
@@ -1,8 +1,12 @@
 package sket.controllers;
 
 import org.json.JSONObject;
+import sket.model.action.RoomAction;
+import sket.model.data.Player;
+import sket.model.data.Room;
 
 import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
 
 public class GameController extends HttpServlet {
 
@@ -11,12 +15,21 @@ public class GameController extends HttpServlet {
     }
 
     /* 랜덤 출제자를 json 으로 반환 */
-    public static String randomExaminerToJSON(String id, int roomId) {
+    public static String randomExaminerToJSON(int roomId) {
+
+        Room targetRoom = RoomAction.findRoomById(roomId);
+        int countUser = targetRoom.getTotalUserNumber();
+
+        int randValue = (int) (Math.random() * countUser) + 1;
+        ArrayList<Player> roomMembers = targetRoom.getPlayerList();
+
+        Player targetPlayer = roomMembers.get(randValue - 1);
+
         JSONObject message = new JSONObject();
-        message.put("type", "randomExaminer");
+        message.put("type", "RANDOM_EXAMINER");
 
         JSONObject data = new JSONObject();
-        data.put("id", id);
+        data.put("id", targetPlayer.getId());
         data.put("roomId", roomId);
 
         message.put("data", data);
